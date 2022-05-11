54a7bf7019d561798714f25299ac02608aed5308
halil <halilerk77@icloud.com>
2022-05-11 14:18:29 +0300

    last-version

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+let saveEl = document.getElementById("save-el");
 let countEl = document.getElementById("count-el");
-
-console.log(countEl);
-
 let count = 0;
+console.log(saveEl);
 function increment() {
   count += 1;
-  countEl.innerText = count;
+  countEl.textContent = count;
+}
+
+function save() {
+  let countStr = count + " - ";
+  saveEl.textContent += countStr;
+  countEl.textContent = 0;
+  count = 0;
 }
